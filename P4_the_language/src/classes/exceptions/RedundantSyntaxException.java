da72161e997520521b0c10be0f71a2c347792013
Alexander <ahha19@student.aau.dk>
2021-05-10 08:33:37 +0200

    fix

--- P4_the_language/src/classes/exceptions/RedundantSyntaxException.java
+++ P4_the_language/src/classes/exceptions/RedundantSyntaxException.java
@@ -11,14 +11,29 @@ public class RedundantSyntaxException extends Exception {
     /**
      * StringType Exceptions
      */
-    public RedundantSyntaxException(String id,  SimpleNode node, String containValue, String exactValue) {
-        super(ContainsSameAsEqualString(id,  node, containValue, exactValue));
+    public RedundantSyntaxException(String id, SimpleNode node, String containValue, String exactValue) {
+        super(ContainsSameAsEqualString(id, node, containValue, exactValue));
+    }
+
+    public RedundantSyntaxException(String id, SimpleNode node, String containValue, String exactValue, String containsContains) {
+        super(ContainsIsContainedInContainsString(id, node, containValue, exactValue, containsContains));
     }
 
     private static String ContainsSameAsEqualString(String id, SimpleNode node, String containValue, String exactValue) {
         PrettyPrinterVisitor ppv = new PrettyPrinterVisitor();
         String newMessage = "WARNING:\n";
         newMessage += "REDUNDANT SYNTAX WARNING: \"" + id + "\" has been given the exact value \"" + exactValue + "\" and it should contain the value \"" + containValue + "\", this is redundant. Therefore one of the operations can be omitted.\n";
+        newMessage += "At line: "; // ikke sikker på om det bliver contains linjen som bliver udskrevet 
+        node.jjtAccept(ppv, null);
+        newMessage += ppv.print;
+        newMessage += "\n";
+        return newMessage;
+    }
+    
+    private static String ContainsIsContainedInContainsString(String id, SimpleNode node, String firstContain, String secondConstrain, String containsContains) {
+        PrettyPrinterVisitor ppv = new PrettyPrinterVisitor();
+        String newMessage = "WARNING:\n";
+        newMessage += "REDUNDANT SYNTAX WARNING: \""+id+"\" should contain both \""+firstContain+"\" and \""+secondConstrain+"\", this is redundant. Therefore one of the operations can be omitted.\n";
         newMessage += "At line: "; // ikke sikker på om det bliver contains linjen som bliver udskrevet 
         node.jjtAccept(ppv, null);
         newMessage += ppv.print;
